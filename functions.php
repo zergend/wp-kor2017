20aa36895b848c5ef000079ea801321bbf6c3c67
zergend <zergend@gmail.com>
2016-12-30 00:08:57 +0300

    29 12 2016 night

--- functions.php
+++ functions.php
@@ -91,6 +91,33 @@ remove_action('wp_head','rsd_link');  // для сервиса Really Simple Dis
 remove_action('wp_head','wlwmanifest_link'); // для Windows Live Writer
 // add_filter( ‘show_admin_bar’, ‘__return_false’ );
 remove_action('wp_head', '_admin_bar_bump_cb'); // убираем админ-панель
+remove_action( 'wp_head', 'wp_resource_hints', 2);
+// Отключаем сам REST API
+add_filter('rest_enabled', '__return_false');
+
+// Отключаем фильтры REST API
+remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10, 0 );
+remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
+remove_action( 'auth_cookie_malformed', 'rest_cookie_collect_status' );
+remove_action( 'auth_cookie_expired', 'rest_cookie_collect_status' );
+remove_action( 'auth_cookie_bad_username', 'rest_cookie_collect_status' );
+remove_action( 'auth_cookie_bad_hash', 'rest_cookie_collect_status' );
+remove_action( 'auth_cookie_valid', 'rest_cookie_collect_status' );
+remove_filter( 'rest_authentication_errors', 'rest_cookie_check_errors', 100 );
+
+// Отключаем события REST API
+remove_action( 'init', 'rest_api_init' );
+remove_action( 'rest_api_init', 'rest_api_default_filters', 10, 1 );
+remove_action( 'parse_request', 'rest_api_loaded' );
+
+// Отключаем Embeds связанные с REST API
+remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+remove_filter( 'rest_pre_serve_request', '_oembed_rest_pre_serve_request', 10, 4 );
+
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+// если собираетесь выводить вставки из других сайтов на своем, то закомментируйте след. строку.
+remove_action( 'wp_head', 'wp_oembed_add_host_js' );
 
 // убираем разные ссылки при отображении поста - следующая, предыдущая запись, оригинальный url и т.п.
 remove_action('wp_head','start_post_rel_link',10,0);
@@ -103,4 +130,23 @@ function remove_wp_version() {
 	return '';
 }
 add_filter('the_generator', 'remove_wp_version');
+/* ----------------------------------------------------------------
+ * Отключаем Emojii
+ * ---------------------------------------------------------------- */
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+add_filter( 'tiny_mce_plugins', 'disable_wp_emojis_in_tinymce' );
+function disable_wp_emojis_in_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
+
 ?>
