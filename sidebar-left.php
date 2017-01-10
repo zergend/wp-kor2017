acae749ff0530a36b653ee42fa0b7e85842f73d7
zergend <zergend@gmail.com>
2017-01-11 00:02:19 +0300

    10 01 2017 night

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,5 +1,6 @@
   <div class="column  column--left">
     <?php
+
     wp_nav_menu( array( // меню
       'theme_location'  => 'sidebar-menu',
       'menu'            => '',
@@ -9,52 +10,37 @@
       'menu_class'      => 'menu-vertical__list',
       'menu_id'         => '',
       'echo'            => true,
-      'fallback_cb'     => 'wp_page_menu',
+      'fallback_cb'     => false,
       'before'          => '',
       'after'           => '',
       'link_before'     => '',
       'link_after'      => '',
       'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
       'depth'           => 0,
-      'walker'          => '',
+      'walker'          => new magomra_walker_nav_menu,
     ) );
+
+    // function magomra_nav_menu( $menu_id ) {
+  	// main navigation menu
+    /*	$args = array(
+    		'theme_location'    => 'sidebar-menu',
+    		'container'     => 'nav',
+    		'container_id'      => '',
+    		'conatiner_class'   => 'menu-vertical',
+    		'menu_class'        => 'menu-vertical__list menu main-menu menu-depth-0 menu-even',
+    		'echo'          => true,
+    		'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+    		'depth'         => 10,
+    		'walker'        => new magomra_walker_nav_menu
+    	); */
+
+  	// print menu
+  	// wp_nav_menu( $args );
+  // }
     ?>
-    
+
     <aside id="first-widget-area" class="widget-area" role="complementary">
       <?php dynamic_sidebar( 'sidebar-1' ); ?>
-    </aside><!-- #secondary -->
+    </aside><!-- #first -->
 
-    <nav class="menu-vertical">
-      <ul class="menu-vertical__list">
-        <li class="menu-vertical__item  current-menu-item">
-          <a href="#">Главная</a>
-        </li>
-        <li class="menu-vertical__item">
-          <a href="#">Администрация</a>
-        </li>
-        <li class="menu-vertical__item  menu-item-has-children">
-          <a href="#">Новости</a>
-          <ul class="menu-vertical__list  submenu">
-            <li class="menu-vertical__item">
-              <a href="#">Подменю 1</a>
-            </li>
-            <li class="menu-vertical__item">
-              <a href="#">Подменю 2</a>
-            </li>
-          </ul>
-        </li>
-        <li class="menu-vertical__item">
-          <a href="#">Дума</a>
-        </li>
-        <li class="menu-vertical__item">
-          <a href="#">Отдел муниципального заказа и потребительского рынка</a>
-        </li>
-        <li class="menu-vertical__item">
-          <a href="#">Пожарная безопасность</a>
-        </li>
-        <li class="menu-vertical__item">
-          <a href="#">Поселения</a>
-        </li>
-      </ul>
-    </nav>
   </div>
