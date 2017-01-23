72e5d0614018422624dc79913a4dccef04433d3d
zergend <zergend@gmail.com>
2017-01-23 16:53:47 +0300

    23 01 2017 evening

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,11 +1,13 @@
   <div class="column  column--left">
+    <nav class="menu-vertical  menu-vertical--closed  menu-vertical--nojs">
+      <button class="menu-vertical__toggle" type="button">Открыть меню</button>
     <?php
 
     wp_nav_menu( array( // меню
       'theme_location'  => 'sidebar-menu',
       'menu'            => '',
-      'container'       => 'nav',
-      'container_class' => 'menu-vertical',
+      'container'       => false,
+      'container_class' => '',
       'container_id'    => '',
       'menu_class'      => 'menu-vertical__list',
       'menu_id'         => '',
@@ -20,6 +22,23 @@
       'walker'          => new walker_vertical_nav_menu
     ) );
     ?>
+    </nav>
+    <script>
+      var navMain = document.querySelector('.menu-vertical');
+      var navToggle = document.querySelector('.menu-vertical__toggle');
+
+      navMain.classList.remove('menu-vertical--nojs');
+
+      navToggle.addEventListener('click', function() {
+        if (navMain.classList.contains('menu-vertical--closed')) {
+          navMain.classList.remove('menu-vertical--closed');
+          navMain.classList.add('menu-vertical--opened');
+        } else {
+          navMain.classList.add('menu-vertical--closed');
+          navMain.classList.remove('menu-vertical--opened');
+        }
+      });
+    </script>
 
     <aside id="first-widget-area" class="widget-area">
       <?php dynamic_sidebar( 'sidebar-1' ); ?>
